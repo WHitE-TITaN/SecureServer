e886e4a1317dcd2f22fe0d05f77387f1834447d1
ADMIRE-WT <dk488621@gmail.com>
2025-12-13 14:27:05 +0530

    remove: correct pathing for classes

--- secureServer/WEB-INF/classes/IndexServlet.java
+++
@@ -1,9 +0,0 @@
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.*;
-
-public class IndexServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.html").forward(request, response);
-    }
-}
