d91b442ff71156292f8fb825feeeb8bdfe82d5f2
ADMIRE-WT <dk488621@gmail.com>
2025-12-13 14:27:48 +0530

    update: updated new structure

---
+++ secureServer/src/main/java/IndexServlet.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang=\"en\">");
+        out.println("<head>");
+        out.println("  <meta charset=\"UTF-8\">");
+        out.println("  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+        out.println("  <title>Document</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("  <h1>Welcome to the Secure Server Landing Page</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
